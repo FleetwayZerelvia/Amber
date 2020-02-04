817f6a3780d4dffa790c6febe4b1a60f59a48721
FleetwayZerelvia <60655014+FleetwayZerelvia@users.noreply.github.com>
2020-02-04 15:47:40 +0100

    presentation command

---
+++ Commands/presentation.js
@@ -0,0 +1,5 @@
+module.exports = connexion.js;
+
+function presCommand(arguments, receivedMessage) {
+    receivedMessage.channel.send("Hello, my name is Amber, i'm a Discord bot created in JavaScript by Fleetway Mystefright Zerelvia#4508")
+}

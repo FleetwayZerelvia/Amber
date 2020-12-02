9a85575f56cfb8b4545ebaec947cd0417b9a6947
Fleetway <fleetwayzerelvia@hotmail.com>
2020-12-02 13:13:27 +0100

    Upgrade of the command to show ping

--- Bot/Foundation/Commands/ping.js
+++ Bot/Foundation/Commands/ping.js
@@ -1,3 +1,11 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
 module.exports = (arguments, receivedMessage) => {
-    receivedMessage.channel.send("Pong!")
+    receivedMessage.channel.send(`Pinging...`).then(m => {
+        var ping = m.createdTimestamp - receivedMessage.createdTimestamp;
+        var botPing = Math.round(bot.pi);
+
+        m.edit(`**:ping_pong: Pong! Your Ping Is:-**\n  ${ping}ms`);
+    });
 }

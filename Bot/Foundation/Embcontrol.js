28dc34d4ce12c6447b0535a2839dafa42685d261
Fleetway <fleetwayzerelvia@hotmail.com>
2020-12-02 17:18:54 +0100

    Updated Embed controller

--- Bot/Foundation/Embcontrol.js
+++ Bot/Foundation/Embcontrol.js
@@ -3,18 +3,30 @@ const Discord = require('discord.js');
 
 // inside a command, event listener, etc.
 const EmbedAmber = new Discord.MessageEmbed()
-	.setColor('#FF2D00')
+	.setColor('#0099ff')
 	.setTitle('Some title')
 	.setURL('https://discord.js.org/')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields({
+		name: 'Regular field title',
+		value: 'Some value here'
+	}, {
+		name: '\u200B',
+		value: '\u200B'
+	}, {
+		name: 'Inline field title',
+		value: 'Some value here',
+		inline: true
+	}, {
+		name: 'Inline field title',
+		value: 'Some value here',
+		inline: true
+	}, )
 	.addField('Inline field title', 'Some value here', true)
 	.setImage('https://i.imgur.com/wSTFkRM.png')
 	.setTimestamp()
 	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
+	
 channel.send(EmbedAmber);

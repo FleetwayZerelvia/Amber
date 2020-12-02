dbaab5efdf0924fa132e3fe51d3470c4202da16a
Fleetway <fleetwayzerelvia@hotmail.com>
2020-12-02 10:29:24 +0100

    Update connexion.js

--- Bot/Foundation/connexion.js
+++ Bot/Foundation/connexion.js
@@ -6,7 +6,7 @@ bot.on('ready', function() {
     console.log("Je suis connecté !");
 });
 
-bot.login('NDI0NjUzNDQ1OTc5NTcwMTc4.Wq1vVw.wuG-CdOZ9gqCh6tuekjAzOhooFk');
+bot.login('xxxxxxxx');
 
 bot.on('message', (receivedMessage) => {
     if (receivedMessage.author == bot.user) { // Prevent bot from responding to its own messages

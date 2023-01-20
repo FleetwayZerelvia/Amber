f7d2125f7de290e3947444968b32e899eae00534
Fleetway <fleetwayzerelvia@hotmail.com>
2023-01-20 19:02:27 +0100

    Merged with old connection file and changed dir.

---
+++ Bot/Foundation/Index.js
@@ -0,0 +1,59 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits, ActivityType } = require('discord.js');
+const { token, clientId } = require('./Core/Config/Config.json');
+
+// Set our intents here.
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+// Load all the commands
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, './Commands')
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+// Load all events
+const eventsPath = path.join(__dirname, 'Events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Print Status
+client.on('ready', () => {
+    date = new Date()
+	console.log(`${date.getMilliseconds()}: ${client.user.username} is ready!`);
+    client.user.setPresence({
+        status: "online",
+        activities: [{
+            name: "With code",
+            type: ActivityType.Playing
+        }]
+    });
+});
+
+client.login(token);
